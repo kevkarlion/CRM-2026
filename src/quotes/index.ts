a6a757d5b11526f76aeed7d3a36e9121ec1e9103
Kevin Riquelme <kriquelme10@gmail.com>
2026-06-21 22:03:58 +0000

    feat(quotes): add public barrel export

---
+++ src/quotes/index.ts
@@ -0,0 +1,6 @@
+export * from './types';
+export * from './schemas';
+export * from './models';
+export { getNextQuoteNumber } from './helpers/counter';
+export { canTransition, validateTransition, validateSendRequirements, validateApproveRequirements, TransitionError, VALID_TRANSITIONS, TERMINAL_STATUSES } from './helpers/state-machine';
+export { processItems, calculateSubtotal, calculateTotal } from './helpers/calculator';
