2f0d450eabb01832c9944c5e3ebf4bfb2f42ac01
Kevin Riquelme <kriquelme10@gmail.com>
2026-06-22 15:05:41 +0000

    feat(quotes): add QuoteService and ConversionService

---
+++ src/quotes/services/index.ts
@@ -0,0 +1,4 @@
+export { QuoteService, ConflictError, ValidationError, NotFoundError } from './quote.service';
+export type { QuoteListFilters, QuoteListResult, CreateQuoteResult, GetQuoteResult, UpdateQuoteResult } from './quote.service';
+export { ConversionService, ConversionError } from './conversion.service';
+export type { ConvertToWorkOrderOptions, ConvertToWorkOrderResult } from './conversion.service';
