62549324556014ac56b4ca7a58689478d7d172e5
Kevin Riquelme <kriquelme10@gmail.com>
2026-06-26 12:52:00 +0000

    feat(pipeline): add stage-mapper utils and barrel index
    - stage-mapper.ts: getTargetStatus(), isValidDropTarget() for DnD validation
    - index.ts: barrel file for pipeline-board module

---
+++ src/leads/pipeline-board/index.ts
@@ -0,0 +1,7 @@
+export { LeadCard } from './components/LeadCard';
+export { PipelineColumn } from './components/PipelineColumn';
+export { ColumnHeader } from './components/ColumnHeader';
+export { DragOverlayCard } from './components/DragOverlayCard';
+export { usePipelineLeads } from './hooks/usePipelineLeads';
+export { usePipelineBoard } from './hooks/usePipelineBoard';
+export { getTargetStatus, isValidDropTarget } from './utils/stage-mapper';
